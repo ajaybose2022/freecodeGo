a1af6aaa81bf0fa83c4183c19556b012df6cba51
ajaybose2022 <ajaybose2022@gmail.com>
2023-11-24 10:42:27 -0500

    Variables and conversion

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
+/*
+	Group the vars together
+
+var (
+
+	actorName    string = "Elizabeth Sladen"
+	companion    string = "Sarah Jane Smith"
+	doctorNumber int    = 3
+	season       int    = 11
+
+)
+*/
 func main() {
 	var i int = 42
-	j := 27
-	var k int
-	k = 67
-	fmt.Println(i)
-	fmt.Println(j)
-	fmt.Println(k)
+	var j float32
+	var k string
+	//var k int -- Not recommended
+	//k = 67
+	fmt.Printf("%v %T\n", i, i)
+	j = float32(i)
+	fmt.Printf("%v %T\n", j, j)
+	k = strconv.Itoa(i)
+	fmt.Printf("%v %T\n", k, k)
+	//fmt.Println(k)
 }
